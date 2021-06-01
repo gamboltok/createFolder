2cc28640d49bf50dfe4cd09549200d017a559d7a
alex <rootssl@mail.ru>
2021-06-01 13:07:55 +0300

    создание папки и файла по таймеру

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+const path = require('path');
+const fs = require('fs');
+
+console.log('wait, i create new folder');
+
+const timerFolder = setTimeout(()=>{
+   createNewFolder('notes');  
+}, 1000);
+
+const timerFile = setTimeout(()=>{
+    createNewFile('boss', 'notes', 'js');  
+}, 3000);
+
+function createNewFolder(name){
+    fs.mkdir(
+        path.join(__dirname, name),
+        err=>{
+            console.log(`Folder ${name.toUpperCase()} created.`);
+            console.log(`a new file is created inside the folder ${name.toUpperCase()}`); 
+        }
+    );
+}
+
+function createNewFile(name, folder, format){
+    fs.writeFile(
+        path.join(__dirname, folder, name + '.' + format),
+        'message',
+        err=>{
+            if(err) throw err;
+            console.log(`file ${name.toUpperCase() + '.' + format.toUpperCase()} created.`);
+        }
+    );
+}
